3fabcdd25a78464292b7eeb78f619aaf3948a9f4
Amer Aouamer <ameraouamer12@gmail.com>
2026-08-11 00:36:28 +0100

    training

--- training/train_model.py
+++ training/train_model.py
@@ -3,6 +3,8 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import StandardScaler 
 from sklearn.model_selection import train_test_split 
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
 
 def prepare_and_scale_data(df: pd.DataFrame, target_column: str= 'status' ):
     """
@@ -20,7 +22,7 @@ def prepare_and_scale_data(df: pd.DataFrame, target_column: str= 'status' ):
     X_train,X_test,y_train,y_test= train_test_split( X , y_encoded, test_size=0.2 ,random_state=42)
 
     #4.scale features 
-    #Initialize and apply the StandardScaler (only fit on training data!)
+    #Initialize and apply the StandardScaler (only fit on training data)
     scaler= StandardScaler()
     X_train_scaled=scaler.fit_transform(X_train)
     X_test_scaled=scaler.transform(X_test)
@@ -30,4 +32,29 @@ def prepare_and_scale_data(df: pd.DataFrame, target_column: str= 'status' ):
     X_test_scaled_df= pd.DataFrame(X_test_scaled, columns=X_test.columns)
 
     return X_train_scaled_df, X_test_scaled_df, y_train, y_test, scaler, le
+
+def train_model(X_train_scaled , y_train):
+    """
+    Trains a random forest clasifier on our scaled data 
+    """
+
+    # 1. Initialize the Model (The "Brain")
+    model= RandomForestClassifier(n_estimators=200, max_depth=10, random_state=42)
+    #2.train the model 
+    print('training the model ...')
+    model.fit(X_train_scaled, y_train)
+    return model
+
+def evaluate_model(model,X_test_scaled,y_test,le):
+    """
+    Tests the model on unseen data and prints a performance report.
+    """
+    print('Evaluating model performance...')
+    predictions=model.predict(X_test_scaled)
     
+    #  Bring the labels back to words so humans can read the report
+    target_names=le.classes_
+    print()
+  
+
+   
